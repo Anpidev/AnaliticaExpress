893c4a8dd4f34b5dd149c450a934d1db41de4a9c
Anpidev <angelaruizdev@gmail.com>
2025-04-23 17:41:37 +0200

    endpoints cambiados a render

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,38 +1,38 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {LoginUsuario} from '../models/login-usuario';
-import {NuevoUsuario} from '../models/nuevo-usuario';
-import {Observable} from 'rxjs';
-import {Usuario} from '../models/usuario';
+import { HttpClient } from '@angular/common/http';
+import { LoginUsuario } from '../models/login-usuario';
+import { NuevoUsuario } from '../models/nuevo-usuario';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private usuarioActual: Usuario | null = null;
-  constructor(private readonly http: HttpClient) {
-  }
+
+  constructor(private readonly http: HttpClient) { }
 
   login(loginUsuario: LoginUsuario) {
-    return this.http.post(`http://localhost:8080/webapi/auth/login`, loginUsuario, {
+    return this.http.post(`https://tfg-m6og.onrender.com/webapi/auth/login`, loginUsuario, {
       withCredentials: true,
     });
   }
 
   register(nuevoUsuario: NuevoUsuario) {
-    return this.http.post(`http://localhost:8080/webapi/auth/register`, nuevoUsuario, {
+    return this.http.post(`https://tfg-m6og.onrender.com/webapi/auth/register`, nuevoUsuario, {
       withCredentials: true,
     });
   }
 
   getDetails(): Observable<Usuario> {
-    return this.http.get<Usuario>('http://localhost:8080/webapi/auth/usuario/detalles', {
+    return this.http.get<Usuario>('https://tfg-m6og.onrender.com/webapi/auth/usuario/detalles', {
       withCredentials: true,
     });
   }
 
-  logout(){
-    return this.http.post(`http://localhost:8080/webapi/auth/logout`, null,{
+  logout() {
+    return this.http.post(`https://tfg-m6og.onrender.com/webapi/auth/logout`, null, {
       withCredentials: true,
     });
   }
